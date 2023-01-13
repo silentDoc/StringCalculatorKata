467d7add20813cd1f4d264a2cf37fec74e5211dc
silentDoc <ramon78t@yahoo.es>
2023-01-13 16:27:25 +0100

    Test #5 - Reject negatives with exception and message

--- StringCalculatorKataTests/StringCalculator_Tests.cs
+++ StringCalculatorKataTests/StringCalculator_Tests.cs
@@ -6,6 +6,18 @@ namespace StringCalculatorKataTests
     public class StringCalculator_ShouldSum
     {
         [TestMethod]
+        public void ShouldReturnSameNumber_IfOneNumberString()
+        {
+            string inputString = "19";
+            int expected = 19;
+            StringCalculator _stringCalculator = new();
+
+            int result = _stringCalculator.Add(inputString);
+
+            Assert.AreEqual(expected, result, "One number string not handled properly");
+        }
+
+        [TestMethod]
         public void ShouldReturnSum_IfTwoNumberString()
         {
             string inputString = "19,27";
@@ -44,18 +56,7 @@ namespace StringCalculatorKataTests
 
             Assert.AreEqual(expected, result, "Empty string not handled properly");
         }
-
-        [TestMethod]
-        public void ShouldReturnSameNumber_IfOneNumberString()
-        {
-            string inputString = "19";
-            int expected = 19;
-            StringCalculator _stringCalculator = new();
-
-            int result = _stringCalculator.Add(inputString);
-
-            Assert.AreEqual(expected, result, "One number string not handled properly");
-        }
+        
 
         [DataTestMethod]
         [DataRow("1\n2,3", 6)]
@@ -76,6 +77,24 @@ namespace StringCalculatorKataTests
             StringCalculator _stringCalculator = new();
             int result = _stringCalculator.Add(input);
             Assert.AreEqual(expected, result, "Empty string not handled properly");
+        }
+
+        [DataTestMethod]
+        [DataRow("//[+]\n1+3+-5", "Negatives not allowed -5")]
+        [DataRow("-1,-3,-2", "Negatives not allowed -1,-3,-2")]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ShouldThrowException_IfNegativeNum(string input, string expected)
+        {
+            StringCalculator _stringCalculator = new();
+            try
+            {
+                int result = _stringCalculator.Add(input);
+            }
+            catch(InvalidOperationException io_ex)
+            {
+                StringAssert.Contains(io_ex.Message, expected);
+            }
+            
         }
 
     }
