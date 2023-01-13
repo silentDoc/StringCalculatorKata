3f7d8ccd31cb4b73e1820a0b5aee041964ebdb14
silentDoc <ramon78t@yahoo.es>
2023-01-13 15:41:41 +0100

    Agregar archivos de proyecto.

---
+++ StringCalculatorKata.Tests/StringCalculator_ShouldSum.cs
@@ -0,0 +1,23 @@
+using StringCalculatorKata;
+using System.Runtime.CompilerServices;
+
+namespace StringCalculatorKata.UnitTests
+{
+    [TestFixture]
+    public class StringCalculator_ShouldSum
+    {
+        private StringCalculatorKata _stringCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            StringCalculator
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ StringCalculatorKataTests/StringCalculator_ShouldSum.cs
@@ -0,0 +1,59 @@
+using StringCalculatorKata;
+
+namespace StringCalculatorKataTests
+{
+    [TestClass]
+    public class StringCalculator_ShouldSum
+    {
+        [TestMethod]
+        public void ShouldReturnSum_IfTwoNumberString()
+        {
+            string inputString = "19,27";
+            int expected = 46;
+            StringCalculator _stringCalculator = new();
+
+            int result = _stringCalculator.Add(inputString);
+
+            Assert.AreEqual(expected, result, "Two number string not handled properly");
+        }
+    }
+    
+    [TestClass]
+    public class StringCalculator_HandleInput
+    {
+        [TestMethod]
+        public void ShouldReturnZero_IfEmptyString()
+        {
+            string inputString = "";
+            int expected = 0;
+            StringCalculator _stringCalculator = new();
+
+            int result = _stringCalculator.Add(inputString);
+
+            Assert.AreEqual(expected, result, "Empty string not handled properly");
+        }
+
+        [TestMethod]
+        public void ShouldReturnSameNumber_IfOneNumberString()
+        {
+            string inputString = "19";
+            int expected = 19;
+            StringCalculator _stringCalculator = new();
+
+            int result = _stringCalculator.Add(inputString);
+
+            Assert.AreEqual(expected, result, "One number string not handled properly");
+        }
+
+        [TestMethod]
+        public void ShouldThrowException_IfNonNumberString()
+        {
+            string inputString = "bla";
+            StringCalculator _stringCalculator = new();
+
+            int result = _stringCalculator.Add(inputString);
+
+            Assert.ThrowsException<System.InvalidOperationException>(() => _stringCalculator.Add(inputString));
+        }
+    }
+}
