0462a33059dd80f47e9beaea9685d8164fdc367c
silentDoc <ramon78t@yahoo.es>
2023-01-13 17:16:42 +0100

    Last refactor not needed - Already covered the cases. Kata completed. Shown some results in console

--- StringCalculatorKata/Program.cs
+++ StringCalculatorKata/Program.cs
@@ -5,7 +5,26 @@
         static void Main(string[] args)
         {
             StringCalculator sc = new();
-            sc.Add("//[+][;][*]\n1+3;5*6*10");
+
+            string input = "1, 2";
+            var result = sc.Add("1, 2");
+            Console.WriteLine(input + " -> " + result.ToString());
+            
+            input = "";
+            result = sc.Add(input);
+            Console.WriteLine(input + " -> " + result.ToString());
+
+            input = "1,2,3,4,5,6";
+            result = sc.Add(input);
+            Console.WriteLine(input + " -> " + result.ToString());
+
+            input = "1\n2,3\n4,5,6";
+            result = sc.Add(input);
+            Console.WriteLine(input + " -> " + result.ToString());
+
+            input = "//[++][;;][***]\n1++3;;5***6***10";
+            result = sc.Add(input);
+            Console.WriteLine(input + " -> " + result.ToString());
         }
     }
 }
